f78c6a69ae5092e6e3e5c9ee2e83fd32f74e9a83
mfw78 <mfw78@protonmail.com>
2022-12-23 07:18:46 +0000

    feat: entrypoint

--- nwaku/entrypoint.sh
+++ nwaku/entrypoint.sh
@@ -1,33 +1,26 @@
 #!/bin/bash
 
-echo "Executing wakunode with the following options:"
-echo "  - public IP address: ${_DAPPNODE_GLOBAL_PUBLIC_IP}"
-echo "  - domain: ${_DAPPNODE_GLOBAL_DOMAIN}"
-
-env
-
 # Execute passing in public IP address and domain
-exec /usr/bin/wakunode --relay=true \
-    --filter=true \
-    --lightpush=true \
-    --store=true \
-    --rpc-admin=true \
-    --keep-alive=true \
-    --max-connections=150 \
-    --dns-discovery=true \
-    --dns-discovery-url=enrtree://AOGECG2SPND25EEFMAJ5WF3KSGJNSGV356DSTL2YVLLZWIV6SAYBM@prod.nodes.status.im \
-    --discv5-discovery=true \
-    --discv5-udp-port=9005 \
-    --discv5-enr-auto-update=true \
-    --log-level=DEBUG \
-    --rpc-port=8545 \
-    --rpc-address=0.0.0.0 \
-    --tcp-port=30304 \
-    --metrics-server=true \
-    --metrics-server-port=8003 \
+exec /usr/bin/wakunode --filter \
+    --lightpush \
+    --store \
+    --rpc-admin \
+    --keep-alive \
+    --max-connections:${MAX_CONNECTIONS:-150} \
+    --dns-discovery \
+    --dns-discovery-url=${DNS_DISCOVERY_URL} \
+    --discv5-discovery \
+    --discv5-udp-port:9005 \
+    --discv5-enr-auto-update \
+    --log-level:${LOG_LEVEL:-DEBUG} \
+    --rpc-port:8545 \
+    --rpc-address:0.0.0.0 \
+    --tcp-port:60000 \
+    --metrics-server \
+    --metrics-server-port=9090 \
     --metrics-server-address=0.0.0.0 \
-    --store-message-db-url=sqlite:///data/store.sqlite3 \
-    --store-message-retention-policy=time:2592000 \
+    --store-message-db-url:sqlite:///data/store.sqlite3 \
+    --store-message-retention-policy:time:${STORE_MESSAGE_RETENTION_POLICY:-2592000} \
     --nat=extip:${_DAPPNODE_GLOBAL_PUBLIC_IP} \
-    --dns4-domain-name=${_DAPPNODE_GLOBAL_DOMAIN} \
+    --dns4-domain-name=nwaku.${_DAPPNODE_GLOBAL_DOMAIN} \
     ${EXTRA_OPTS}
